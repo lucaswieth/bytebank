f384dd4ffb0decffb6def2630ab3bf709e36c46a
Lucas Wieth <lucasewieth@gmail.com>
2024-01-24 15:55:28 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:bytebank/screens/form.dart';
+//import 'package:bytebank/screens/form.dart';
 import 'package:bytebank/screens/list.dart';
 
 void main() {
